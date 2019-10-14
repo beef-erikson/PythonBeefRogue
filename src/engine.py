e33881897cfb6da153a4f2948cda8f745db3da78
Troy Martin <beef.erikson.studios@gmail.com>
2019-10-14 02:58:53 -0400

    Added TODO's for depreciations

--- src/engine.py
+++ src/engine.py
@@ -16,6 +16,8 @@ def main():
 
     # Sets window parameters
     tcod.console_init_root(screen_width, screen_height, game_title, False, tcod.RENDERER_OPENGL2, vsync=True)
+
+    # TODO fix depreciation - check tcod.console.Console(...)
     con = tcod.console_new(screen_width, screen_height)
 
     # Variables for keyboard and mouse inputs
@@ -23,12 +25,17 @@ def main():
     mouse = tcod.Mouse()
 
     # Main game loop
+    # TODO fix depreciation - use the event system.
+    #  See: https://buildmedia.readthedocs.org/media/pdf/python-tcod/latest/python-tcod.pdf page 33
     while not tcod.console_is_window_closed():
+        # TODO fix depreciation - tcod.event.get to check events
         tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
 
         # Draws player
+        # TODO fix depreciation - set con.default_fg attribute instead
         tcod.console_set_default_foreground(con, tcod.white)
         tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
+        # TODO fix depreciation - apparently it's now Console.blit?
         tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         tcod.console_flush()
 
